a1818fa6f04437ef9397c727ebcad576f8c0d6f6
Carol Hew <112550947+sevenpluseight@users.noreply.github.com>
2025-10-30 15:00:38 +0800

    add entities header for shared data structures in hospital modules

---
+++ utils/entities.hpp
@@ -0,0 +1,17 @@
+#ifndef ENTITIES_H
+#define ENTITIES_H
+
+#include <string>
+using namespace std;
+
+/**
+ * @brief Defines shared data structures (entities) used across all hospital modules
+ *
+ * @note
+ * - Do not implement any logic or functions here - only struct declarations
+ * - Include this header in all role modules that need shared entity access
+ * - Update this file CAREFULLY if attributes are modified as it impacts all modules
+ * - If grows too big, can refactor into sub-headers
+ */
+
+#endif
